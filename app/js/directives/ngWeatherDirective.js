22e4bce4b705aab29db1317ffbb9085c080ca839
KerryMaths <kerry.mathurin@gmail.com>
2015-12-09 00:11:53 +0000

    updating responsive

--- app/js/directives/ngWeatherDirective.js
+++ app/js/directives/ngWeatherDirective.js
@@ -44,6 +44,7 @@
 		var img = jQuery('#weatherIcon');//select specific icon
 		var br = jQuery('.brHide');
 		var body = jQuery('body');
+		var weatherDescr = jQuery('#weatherDescr');
 
 		//find name of city & if its different to previous change
 		var prevSearch;
@@ -60,40 +61,153 @@
 		 return previousMain;
 		};
 
-		var switchwWatherMainIcon = function() {
-					switch(scope.weather.weather[0].main.toLowerCase()){
-					 	case 'clear':
-					 		body.css({'background-image': 'url("img/bkg.jpeg")'});
-					 	  img.attr('src','img/sunIcon.png');
-					 	  br.show();
-					 		break;
-					 	case 'clouds':
-					 		body.css({'background-image': 'url("img/cloudyBkg.jpeg")'});
-					 	  img.attr('src','img/cloudIcon.png').fadeIn('slow');
-					 	  br.hide();
-					 		break;
-					 	case 'rain':
-					 		body.css({'background-image': 'url("img/rainBkg.jpeg")'});
-					 	  img.attr('src','img/rainIcon.png');
-					 	  br.show();
-					 		break;
-					 	case 'drizzle':
-					 		body.css({'background-image': 'url("img/rainBkg.jpeg")'});
-					 	  img.attr('src','img/rainIcon.png');
-					 	  br.show();
-					 		break;
-					 	case 'snow':
-					 		body.css({'background-image': 'url("img/snowBkg.jpeg")'});
-					 	  img.attr('src','img/snowIcon.png');
-					 	  br.show();
-					 		break;	
-					 	default:
-					 		body.css({'background-image': 'url("img/cloudyBkg.jpeg")'});
-					 		img.attr('src','img/cloudIcon.png');
-					 		br.hide();
-					 		break;
-					 }
-				 };
+		var switchwWeatherMainIcon = function() {
+			var now = new Date();
+			var getTime = now.getTime();
+			var sunset = scope.weather.sys.sunset;
+			if (getTime > sunset){
+				//if its past sunset display night icons
+				console.log(previousMain);
+				switch(scope.weather.weather[0].main.toLowerCase()){
+					case 'clear':
+				 		body.css({'background-image': 'url("img/bkg.jpeg")'});
+						weatherDescr.css({'padding-top': '150px'});
+				 	  img.attr('src','img/clearNightIcon.png');
+				 	  br.show();
+				 		break;
+				 	case 'clouds':
+				 		body.css({'background-image': 'url("img/cloudyBkg.jpeg")'});
+						weatherDescr.css({'padding-top': '150px'});
+				 	  img.attr('src','img/cloudyNightIcon.png').fadeIn('slow');
+				 	  br.show();
+				 		break;
+				 	case 'fog':
+				 		body.css({'background-image': 'url("img/mistBkg.jpeg")'});
+						weatherDescr.css({'padding-top': '150px'});
+				 	  img.attr('src','img/mistNightIcon.png').fadeIn('slow');
+				 	  br.show();
+				 		break;
+				 	case 'mist':
+				 		body.css({'background-image': 'url("img/mistBkg.jpeg")'});
+						weatherDescr.css({'padding-top': '150px'});
+				 	  img.attr('src','img/mistNightIcon.png').fadeIn('slow');
+				 	  br.show();
+				 		break;
+				 	case 'smoke':
+				 		body.css({'background-image': 'url("img/mistBkg.jpeg")'});
+						weatherDescr.css({'padding-top': '150px'});
+				 	  img.attr('src','img/mistNightIcon.png').fadeIn('slow');
+				 	  br.show();
+				 		break;
+				 	case 'haze':
+				 		body.css({'background-image': 'url("img/mistBkg.jpeg")'});
+						weatherDescr.css({'padding-top': '150px'});
+				 	  img.attr('src','img/mistNightIcon.png').fadeIn('slow');
+				 	  br.show();
+				 		break;
+				 	case 'rain':
+				 		body.css({'background-image': 'url("img/rainBkg.jpeg")'});
+				 		weatherDescr.css({'padding-top': '200px'});
+				 	  img.attr('src','img/rainNightIcon.png');
+				 	  br.show();
+				 		break;
+				 	case 'drizzle':
+				 		body.css({'background-image': 'url("img/rainBkg.jpeg")'});
+				 		weatherDescr.css({'padding-top': '200px'});
+				 	  img.attr('src','img/rainNightIcon.png');
+				 	  br.show();
+				 		break;
+				 	case 'hail':
+				 		body.css({'background-image': 'url("img/hailBkg.jpeg")', 'background-repeat': 'no-repeat', 'background-size': '100%'});
+				 		weatherDescr.css({'padding-top': '200px'});
+				 	  img.attr('src','img/hailNightIcon.png');
+				 	  br.show();
+				 		break;
+				 	case 'snow':
+				 		body.css({'background-image': 'url("img/snowBkg.jpeg")'});
+						weatherDescr.css({'padding-top': '200px'});
+				 	  img.attr('src','img/snowNightIcon.png').fadeIn('slow');
+				 	  br.show();
+				 	  break;
+				 	 case 'thunderstorm':
+				 		body.css({'background-image': 'url("img/stormBkg.jpeg")'});
+						weatherDescr.css({'padding-top': '200px'});
+				 	  img.attr('src','img/stormNightIcon.png').fadeIn('slow');
+				 	  br.show();
+				 		break;
+				 	default:
+				 		body.css({'background-image': 'url("img/cloudyBkg.jpeg")'});
+						weatherDescr.css({'padding-top': '200px'});
+				 		img.attr('src','img/cloudIcon.png');
+				 		br.hide();
+				 		break;
+				}
+			}
+			else {
+				switch(scope.weather.weather[0].main.toLowerCase()){
+				 	case 'clear':
+				 		body.css({'background-image': 'url("img/bkg.jpeg")'});
+				 	  img.attr('src','img/sunIcon.png');
+				 	  br.show();
+				 		break;
+				 	case 'clouds':
+				 		body.css({'background-image': 'url("img/cloudyBkg.jpeg")'});
+				 	  img.attr('src','img/cloudIcon.png').fadeIn('slow');
+				 	  br.hide();
+				 		break;
+				 	case 'rain':
+				 		body.css({'background-image': 'url("img/rainBkg.jpeg")'});
+				 	  img.attr('src','img/rainIcon.png');
+				 	  br.show();
+				 		break;
+				 	case 'drizzle':
+				 		body.css({'background-image': 'url("img/rainBkg.jpeg")'});
+				 	  img.attr('src','img/rainIcon.png');
+				 	  br.show();
+				 		break;
+				 	case 'mist':
+				 		body.css({'background-image': 'url("img/mistBkg.jpeg")'});
+				 	  img.attr('src','img/mistIcon.png').fadeIn('slow');
+				 	  br.hide();
+				 		break;
+				 	case 'fog':
+				 		body.css({'background-image': 'url("img/mistBkg.jpeg")'});
+				 	  img.attr('src','img/mistIcon.png').fadeIn('slow');
+				 	  br.hide();
+				 		break;
+				 	case 'haze':
+				 		body.css({'background-image': 'url("img/mistBkg.jpeg")'});
+				 	  img.attr('src','img/mistIcon.png').fadeIn('slow');
+				 	  br.hide();
+				 		break;
+				 	case 'smoke':
+				 		body.css({'background-image': 'url("img/mistBkg.jpeg")'});
+				 	  img.attr('src','img/mistIcon.png').fadeIn('slow');
+				 	  br.hide();
+				 		break;
+				 	case 'snow':
+				 		body.css({'background-image': 'url("img/snowBkg.jpeg")'});
+				 	  img.attr('src','img/snowIcon.png');
+				 	  br.show();
+				 		break;
+				 	case 'hail':
+				 		body.css({'background-image': 'url("img/hailBkg.jpeg")', 'background-repeat': 'no-repeat', 'background-size': '100%'});
+				 	  img.attr('src','img/hailIcon.png');
+				 	  br.show();
+				 		break;
+				 	case 'thunderstorm':
+				 		body.css({'background-image': 'url("img/stormBkg.jpeg")', 'background-repeat': 'no-repeat', 'background-size': '100%'});
+				 	  img.attr('src','img/stormIcon.png');
+				 	  br.show();
+				 		break;	
+				 	default:
+				 		body.css({'background-image': 'url("img/cloudyBkg.jpeg")'});
+				 		img.attr('src','img/cloudIcon.png');
+				 		br.hide();
+				 		break;
+				}
+			}
+		};
 
 		var loader = function(){
 		 	if (scope.weather.name !== ''){
@@ -101,7 +215,7 @@
 				jQuery('.loader').show();
 				jQuery('input').show();
 			}
-		}
+		};
 
 		var geoLoc = function(){
 		 	navigator.geolocation.getCurrentPosition(showPosition);
@@ -114,7 +228,7 @@
 			 .success(function(data){
 			 	scope.weather = data;
 			 	getPrevSearch();
-				switchwWatherMainIcon();
+				switchwWeatherMainIcon();
 				loader();
 		 });
 		 };
@@ -143,7 +257,7 @@
 					getPrevSearch();
 				}
 
-			 	switchwWatherMainIcon();
+			 	switchwWeatherMainIcon();
 			 	loader();
 
 				 if (scope.weather.weather[0].main.toLowerCase() === previousMain){
